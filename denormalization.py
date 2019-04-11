a018a63b7017f39e1591e0e86c0537b0d9d7f025
amitmakashir <amitm@bigrattle.com>
2019-04-11 19:50:58 -0400

    Denormalized coordinates and bounding boxes

---
+++ denormalization.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 16:22:15 2019
+
+@author: Amit
+"""
+
+def denormalize_coordinates(image_dict,normalized_coords,grid_row_no,grid_col_no,total_grid_rows,total_grid_cols):
+    '''
+    b.b => bounding box
+    
+    The output of feedforward would be of the dimensions: 
+        # of gridrows x # of gridcols x v
+        v = prob. of obj + (4 b.b params) + # of classes
+        
+    This function returns the coordinates for the center of a b.b
+    
+    The inputs for this function are:
+        1. image_dict => represents the size of the image
+        2. normalized_coords {x,y} => normalized coordinates between (0,1) 
+        3. grid_row_no => The row to which this particular grid cell belongs to
+        4. grid_col_no => The col to which this particular grid cell belongs to
+        5. total_grid_rows => Total grid cells in a row
+        6. total_grid_cols => Total grid cells in a col
+    '''
+    cell_height = image_dict["height"] / total_grid_rows
+    cell_width = image_dict["width"] / total_grid_cols
+    
+    grid_cell_topleft = {}
+    grid_cell_topleft["x"] = grid_row_no*cell_height
+    grid_cell_topleft["y"] = grid_col_no*cell_width
+    
+    denormalized_coord = {}
+    denormalized_coord["x"] = grid_cell_topleft["x"] + (cell_width*normalized_coords["x"])
+    denormalized_coord["y"] = grid_cell_topleft["y"] + (cell_height*normalized_coords["y"])
+    
+    return denormalized_coord
+
+
+def denormalize_box_dimension(image_dict,normalized_dimensions):
+    '''
+    This function returns the width and height of the b.b
+    
+    The inputs for this function are:
+        1. image_dict => represents the size of the image
+        2. normalized_box_dimensions {width,height} => normalized box dimensions between (0,1) 
+    '''
+    denormalized_dimensions = {}
+    
+    # box_width/image_width
+    denormalized_dimensions["width"] = normalized_dimensions["width"]*image_dict["width"]
+    denormalized_dimensions["height"] = normalized_dimensions["height"]*image_dict["height"]
+    
+    return denormalized_dimensions
+    
+    
+    
